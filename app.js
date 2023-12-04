a600c0958b70e60d611a3e4e5fab364080b3dfce
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-12-05 00:09:08 +0200

    add 1,2,3,4

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+
 const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
@@ -24,6 +29,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,25 @@
+// updateAvatar
+const fs = require("fs/promises");
+const path = require("path");
+
+const { User } = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  console.log(req.user);
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const extention = originalname.split(".").pop();
+  const filename = `${_id}.${extention}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+module.exports = updateAvatar;
